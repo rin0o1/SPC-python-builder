6ddbe026bf8f7e3e36eb39ec6f0d1054f41b3a0b
Francesco Rinaldi <frarino1002@gmail.com>
2023-03-26 11:00:03 +0100

    commenting builder on step variable declaration

--- BuilderSPC.py
+++ BuilderSPC.py
@@ -29,24 +29,62 @@ class BuilderSPC:
         print(f'{self.log} spc exported.')    
 
     def onStepVariableDeclaration(self, lns, pnt):
+        # Initialising the spc variable used to store 
+        # the SPC code as a Python string.
         spc = ""
+        # Get the options from the BuilderOptions.py.
+        # The variables within this object are set from the
+        # user through the parameters on running the script.
         nco = self.o.nco 
-        l0s = self.parseL0s() if self.o.l0 > 0 else self.generateL0s()
-        nco = self.o.nco
+        # 2D list containing the x,y location for each complex 
+        # object.
+        # It uses the initial locations for each complex object
+        # if set by the user, generate them randomly otherwise.
+        # l0s for 2 complex objects which x,y initial locations 
+        # are 3,2 and 4,1 does have the following content: 
+        # [[3,2], [4,1]].
+        l0s = self.parseL0s() if self.o.l0 > 0 \
+            else self.generateL0s()
         m = self
+        # add comments in the SPC script. 
+        # m.n and m.t refer to self.n and self.n used for common 
+        # characters.
+        # self.t(n) returns a string with n tabs characters. 
+        # eg. self.t(2) returns "\t\t".
+        # this simplifies indentation when generating the spc file.
+        # self.n return a string with a new line character. 
+        # eg. self.n() return "\n".
         spc += f'// {m.t(2)} ++++++++ Code generated from the Builder ++++++++  {m.n()}'
         spc += f'// =================== ON STEP VARIABLE DECLARATION ================== {m.n()}'
+        # For each complex object.
         for i in range(nco):
+            # Create an id for the complex object. 
             id = str(i+1)
+            # Getting the x,y initial location for the i_th 
+            # complex object.
             lx0 = l0s[i][0]
             ly0 = l0s[i][1]
+            # Store the x,y initial location lx0,ly0 for the i_th 
+            # complex object as a string. This will help in the next
+            # lines to use these values for writing the SPC code 
+            # as a string.
             x = f'X_{id}'
             y = f'Y_{id}'
+            # Writing the SPC code for declaring the initial 
+            # locations for the i_th complex object
+            # using Python string.
             spc += f'{m.t(1)}{x} = {lx0} ; {m.n()}'
             spc += f'{m.t(1)}{y} = {ly0} ; {m.n()}' 
+            # Writing the SPC code for declaring the observers 
+            # as Python strings
             spc += f'{m.t(1)}{x}_obv:observe:{x};{m.n()}'
             spc += f'{m.t(1)}{y}_obv:observe:{y};{m.n()}'
+        # write a comment 
         spc += f'//================================================================== {m.n()}'
+        # lns contains the full SPC script as a string.
+        # pnt is a pointer indicating at which point of lns the 
+        # content within the spc string has to be inserted.
+        # Inserting the spc content in lns at line pnt
         lns.insert(pnt, spc)
     
     def onStepStepwise(self, lns, pnt):
